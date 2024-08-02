4fb47f228535e3b6e897a86bc71b0d0b16d8379e
Andre luiz <adventureandre@hotmail.com>
2024-08-01 21:13:09 -0300

    feat: initializing the creation of the reducer Player

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,30 +1,33 @@
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
+import * as Collapsible from '@radix-ui/react-collapsible';
 
-interface ModuleProps{
-    moduleIndex:number
+interface ModuleProps {
+    moduleIndex: number
     title: string
-    amountOfLessons:number
+    amountOfLessons: number
 }
 
-export function Module({amountOfLessons,moduleIndex,title}:ModuleProps) {
+export function Module({ amountOfLessons, moduleIndex, title }: ModuleProps) {
     return (
-        <div>
-            <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+        <Collapsible.Root className="group">
+            <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
                 <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">{moduleIndex + 1}</div>
 
                 <div className="flex flex-col gap-1 text-left">
                     <strong>{title}</strong>
                     <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
                 </div>
-                <ChevronDown className="w-5 h-6 ml-auto text-zinc-400" />
-            </button>
+                <ChevronDown className="w-5 h-6 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+            </Collapsible.Trigger>
 
-            <nav className="relative flex flex-col gap-4 p-6">
-                <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
-                <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
-                <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
-            </nav>
-        </div>
+            <Collapsible.CollapsibleContent>
+                <nav className="relative flex flex-col gap-4 p-6">
+                    <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
+                    <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
+                    <Lesson title={"Fundamentos do Redux"} duration={"09:30"} />
+                </nav>
+            </Collapsible.CollapsibleContent>
+        </Collapsible.Root>
     )
 }
