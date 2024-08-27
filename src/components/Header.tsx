4052adf85c7d4ea13d6bfa965c4d8e7adf3e0052
Andre luiz <adventureandre@hotmail.com>
2024-08-26 23:12:17 -0300

    feat: migration for Redux to the Zustand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
-import { useAppSelector } from "../store"
-import { useCurrentLesson } from "../store/slices/player"
+
+
+import { useCurrentLesson, useStore } from "../zustand-store"
 
 export function Header() {
 
-    const {currentLesson, currentModule} = useCurrentLesson()
+    const { currentLesson, currentModule } = useCurrentLesson()
 
-   const isCourseLoading = useAppSelector(state => state.player.isLoading)
 
-   if(isCourseLoading){
-    return <h1 className="text-2xl font-bold">Carregando...</h1>
-   }
+    const  isLoading  = useStore(store => store.isLoading)
+
+    if (isLoading) {
+        return <h1 className="text-2xl font-bold">Carregando...</h1>
+    }
 
     return (
         <div className="flex flex-col gap-1">
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,8 @@
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import * as Collapsible from '@radix-ui/react-collapsible';
-import { useAppDispatch, useAppSelector } from "../store";
 import { play } from "../store/slices/player";
+import { useStore } from "../zustand-store";
 
 interface ModuleProps {
     moduleIndex: number
@@ -12,16 +12,13 @@ interface ModuleProps {
 
 export function Module({ amountOfLessons, moduleIndex, title }: ModuleProps) {
 
-    const dispatch = useAppDispatch()
-
-    const { currentLessonIndex, currentModuleIndex } = useAppSelector(state => {
-        const { currentLessonIndex, currentModuleIndex } = state.player
-
-        return { currentLessonIndex, currentModuleIndex }
-    })
-
-    const lessons = useAppSelector(state => {
-        return state.player.course?.modules[moduleIndex].lessons
+    const {currentLessonIndex,currentModuleIndex, play, lessons} = useStore(store => {
+        return {
+            lessons: store.course?.modules[moduleIndex].lessons,
+            currentLessonIndex: store.currentLessonIndex,
+            currentModuleIndex: store.currentModuleIndex,
+            play: store.play
+        }
     })
 
     return (
@@ -45,7 +42,7 @@ export function Module({ amountOfLessons, moduleIndex, title }: ModuleProps) {
                                 key={lesson.id}
                                 title={lesson.title}
                                 duration={lesson.duration}
-                                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                                onPlay={() => play([moduleIndex, lessonIndex])}
                                 isCurrent={isCurrent}
                             />
                         )
